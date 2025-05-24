bbbb5244183d15c3ea9fb09d1de935d2e6e197fc
Neel Chaudhary <neilchaudhary12@gmail.com>
2025-05-25 02:23:39 +0530

    added utils

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -5,3 +5,6 @@ import clsx from "clsx";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const isNullOrUndefined = (value: unknown) =>
+  value === null || value === undefined;
