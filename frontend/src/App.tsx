4d0ba40793db83d245fe2f26de131f4e6fdba0b8
Neel Chaudhary <neilchaudhary12@gmail.com>
2025-05-25 02:59:57 +0530

    updated sonner styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,26 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import { Toaster } from "sonner";
 import { Landing } from "./pages/Landing";
+import { Interview } from "./pages/Interview";
 
 function App() {
   return (
     <BrowserRouter>
-      <Toaster richColors position="top-center" />
+      <Toaster
+        position="bottom-right"
+        className="toaster group"
+        toastOptions={{
+          classNames: {
+            toast:
+              "group toast group-[.toaster]:!bg-black group-[.toaster]:!text-white group-[.toaster]:border-border group-[.toaster]:shadow-lg",
+            description: "group-[.toast]:text-gray-300",
+          },
+        }}
+        theme="dark"
+      />
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/interview" element={<Interview />} />
       </Routes>
     </BrowserRouter>
   );
